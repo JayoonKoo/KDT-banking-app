ba141a9517cc5a5a87ad3d0b3661324f641f34b8
JayoonKoo <rnwkdbs12@gmail.com>
2021-09-13 21:37:13 +0900

    feat: 일일 내역 추가
    일일 내역 반환하는 메서드 추가

--- js/data.js
+++ js/data.js
@@ -12,4 +12,24 @@ export default class Data {
 	get bankList() {
 		return this._bankList;
 	}
+
+	getDaliyHistory(ago) {
+		const queryDate = this.getQueyrDate(ago);
+		return this._bankList.bankList.filter(e=> {
+			if (e.date === queryDate) {
+				return true;
+			}
+		})
+	}
+
+	getQueyrDate(ago) {
+		const now = new Date();
+		const [year, month, date] = [now.getFullYear(), now.getMonth(), now.getDate()];
+
+		const agoDate = new Date(year, month, date + ago);
+		const [agoYear, agoMonth, agoDay] = [agoDate.getFullYear(), agoDate.getMonth(), agoDate.getDate()]
+		const queryDate = `${agoYear}-${String(agoMonth + 1).padStart(2, "0")}-${String(agoDay).padStart(2, "0")}`;
+
+		return queryDate
+	}
 }
