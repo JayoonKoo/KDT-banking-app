d9d809222eb90cd99fbe5df8312567ebe5d08e9c
JayoonKoo <rnwkdbs12@gmail.com>
2021-09-14 17:31:03 +0900

    feat: 작업 중단

---
+++ js/home.js
@@ -0,0 +1,75 @@
+import Api from "./api.js";
+
+export default class Home {
+	_state;
+	_api;
+	_container;
+
+	constructor(data) {
+		this._container = document.querySelector(".transaction-histories");
+		this._container.innerHTML = "";
+		this._state = data;
+		this._api = new Api();
+	}
+
+	async render() {
+		if (this._state.bankList.length === 0) {
+			this._state.bankList = await this._api.request();
+		}
+		for (let i=0; i>-10; i--) {
+			const transactionDaily = document.createElement('li');
+			transactionDaily.className = "transaction-daily";
+
+			let date = "";
+			if (i === 0) {
+				date = "오늘";
+			} else if (i === -1) {
+				date = "어제";
+			} else {
+				date = `${-1*i}일전`;
+			}
+
+			const dailyHistory = this._state.getDaliyHistory(i);
+			const cost = this.getCost(dailyHistory);
+
+			const transaction = document.createElement("ul");
+			transaction.className = "transactions";
+
+			const dailyCost = this.createDailyCost(date, cost);
+			const transactioItem = this.createTransactionItem(dailyHistory).join('');
+			transactionDaily.innerHTML = [dailyCost, transactioItem].join('');
+
+			this._container.appendChild(transactionDaily);
+		}
+	}
+
+	createDailyCost(date, cost) {
+		const transaction = `
+			<div class="daily-cost">${date}</div>
+			<span class="cost">
+				<span class="cost-sum">${cost}</span> 원 지출
+			</span>
+		`
+
+		return transaction
+	}
+
+	createTransactionItem(dailyHistory) {
+		return dailyHistory.map(data => {
+			const {income, history, price} = data;
+			return (`
+				<li class="transaction-item>
+					<span class="item-name">${history}</span>
+					<span class="item-cost ${income}">${price}</span>
+				</li>
+			`);
+		})
+	}
+
+	getCost(dailyHistory) {
+		let cost = 0
+		dailyHistory.forEach(item => cost += item.price);
+		return cost;
+	}
+}
+
